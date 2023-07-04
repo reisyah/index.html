197db785d7592ce08a98b261120396e6515e33ce
reisyah <138543445+reisyah@users.noreply.github.com>
2023-07-04 13:57:18 +0800

    Create function.js

---
+++ function.js
@@ -0,0 +1,385 @@
+
+function nsch5() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:nsch5";
+}
+
+
+function nsch6() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:nsch6";
+}
+function nschfree() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:nschfree";
+}
+
+function nsch205() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:nsch205";
+}
+
+function yt1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://live.fc2.com/embedPlayer.php?id=72893726&lang=en&adultaccess=0&suggest=1&thumbnail=1&afid=72893726";
+        }
+ 
+function yt2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://www.youtube.com/embed/XZ-E0Qk3N7c";
+        }
+ 
+function yt3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://live.fc2.com/embedPlayer.php?id=79129629&lang=en&adultaccess=0&suggest=1&thumbnail=1&afid=0";
+        }
+ function yt4() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://play.afreecatv.com/putra4/embed";
+        }
+
+    function yt5() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://lsbtv3.livestreaming.repl.co/player2.html?file=https://sagalada.shop/live/sd-1-3937822.m3u8";
+   }
+   function pidio1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player.html?file=https://vsd132.mycdn.me/hls/4985862425202.m3u8/sig/V0LXcIVOAFI/expires/1687696572664/srcIp/140.213.226.33/urls/45.136.21.57/clientType/0/srcAg/CHROME_ANDROID/mid/6539022900850/4985862425202_high/index.m3u8?";
+   }
+   function pidio2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://deptokv1.herokuapp.com/index.php?id=13448";
+   }
+https://arjunaaaaa.github.io/nyuri/pemutar/player.html?file=https://deptokv1.herokuapp.com/index.php?id=13226
+function sd() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://lsbtv3.livestreaming.repl.co/player1.html?file=https://etslive-2-vidio-com.akamaized.net/exp=1687095634~acl=/hls-p/ingest_13097_*/*~hmac=51a87608178c4826f176cc7467718d2cbb85e2a273533f75cd4468ca3203e047/hls-p/ingest_13097_360p/index.m3u8";
+}
+
+   function hd() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://v3.sportsonline.sx/channels/hd/hd2.php";
+   }
+function hd2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://v3.sportsonline.sx/channels/hd/hd2.php";
+   }
+function hd6() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://v3.sportsonline.sx/channels/hd/hd6.php";
+   }
+   
+function p2kzb1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://sagalada.shop/live/sd-1-3875356.m3u8";
+        }
+function p2kzb2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://daddylive.herokuapp.com/index.m3u8?id=premium125&apiv2=ddy1";
+        }
+
+function p2kzb3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://sagalada.shop/live/sd-1-3932921.m3u8";
+        }
+function p2kzb4() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://daddylive.herokuapp.com/index.m3u8?id=premium125&apiv2=ddy1";
+}
+
+function p2thapcam1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/thapcamtv/G0GknSA.m3u8";
+}
+
+function p2thapcam2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/thapcamtv/dmjAPfK.m3u8";
+}
+function p2thapcam3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/thapcamtv/dmjAPfK.m3u8";
+}
+
+function p2thapcam4() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/thapcamtv/dmjAPfK.m3u8";
+}
+
+function p2soco1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/socolive/479379.m3u8";
+}
+
+function p2soco2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/socolive/991109.m3u8";
+}
+
+
+function p2soco3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/socolive/344687.m3u8";
+}
+
+function p2soco4() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/socolive/991135.m3u8";
+}
+    function p2soco5() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/socolive/208390.m3u8";
+}    
+        function vidiochlow() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vidiolow";
+        }
+    function vidiochsd() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vidiosd";
+        }    
+function vidiochhd() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vidiohd";
+        }
+function vidiofhd() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vidiofhd1";
+        }
+
+
+function vidiochlow2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vidiochlow";
+        }
+    function vidiochsd2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vidiochsd";
+        }    
+function vidiochhd2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vidiochhd";
+        }
+function vidiofhd2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vidiofhd";
+        }
+        
+
+
+   function kzb71() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:kzb71";
+        }
+function kzb72() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:kzb72";
+        }
+function kzb73() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:kzb73";
+        }
+function cak1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:cak1";
+        }
+
+function cak2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:cak2";
+}
+
+     function cak3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:cak3";
+        }   
+function cak4() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:cak4";
+        }
+
+function vebotv1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vebotv1";
+        }
+        
+   function vebotv2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vebotv2";
+        }
+function vebotv3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:vebotv3";
+        }
+function thapcamtv1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:thapcamtv1";
+        }
+  function thapcamtv2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:thapcamtv2";
+        }
+function thapcamtv3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:thapcamtv3";
+        }
+function livesport1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:livesport1";
+        }
+  function livesport2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:livesport2";
+        }
+function livesport3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:808web1";
+        }
+        
+     function rbtv1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:rbtv1";
+        }   
+        
+     function rbtv2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:rbtv2";
+        }   
+     
+        
+     function rbtv3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:rbtv3";
+        }   
+     
+
+
+
+     function antv1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:antvyt";
+        }   
+      function antv2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:antv";
+        }
+function go3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:s4il";
+        }
+      function gorcti() {
+            var frame = document.getElementById("vidio");
+            frame.src = "go:rcti2";
+        }
+        
+function c1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://vku.youku.com/live/ilpshare?id=8150964";
+        }
+ function c2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://vku.youku.com/live/ilpshare?id=8150965";
+        }
+ function c3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://vku.youku.com/live/ilpshare?id=8150966";
+        }
+ 
+
+   
+  function c4() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://vku.youku.com/live/ilpshare?id=8150967";
+        }
+ 
+function mncsp1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://pisionplus.livebola.repl.co/mncsport.html";
+        }
+  function sportdigital() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/tv/olahraga/sportdigital.html";
+        }    
+   function ssc1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/tv/olahraga/ssc1.html";
+        }        
+        
+  function tycs() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://arjunaaaaa.github.io/nyuri/tv/olahraga/tycs.html";
+        }          
+ 
+ function mncsp2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://mncsport2.livestreaming.repl.co";
+        }
+   function mncsp3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://mncsport3.livestreaming.repl.co";
+   }
+ 
+function spotv2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://spotv2.livestreaming.repl.co";
+        }
+ 
+ function spotv1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://pisionplus.livebola.repl.co/spotv.html";
+        }
+ 
+ function spotvon() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://spotvon--bolaboka.repl.co";
+        }
+ function kzb7() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://player.huminbird.cn/detail.html?v=109&mid=3924185&type=1&pid=4&isTips=1&isLogin=0&sbtcolor=27c5c3&pfont=40px";
+        }
+ function polsatprem1() {
+            var frame = document.getElementById("vidio");
+            iframe.src = "https://polsatprem1.bolaboka.repl.co/";
+        }
+ function ten2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://player4.bolaboka.repl.co/index.html?file=https://dai.google.com/linear/hls/event/LK-ik89MQIi_pWBbg74KNQ/master.m3u8";
+        }
+        function ten3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://player4.bolaboka.repl.co/index.html?file=https://dai.google.com/linear/hls/event/BCOFZq1JQjq12fmaO6lAAA/master.m3u8";
+        }
+        function ten4() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://player4.bolaboka.repl.co/index.html?file=https://dai.google.com/linear/hls/event/smYybI_JToWaHzwoxSE9qA/master.m3u8";
+        }
+        
+function soccerch() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://pisionplus.livebola.repl.co/soccerch.html";
+        }
+     function mewatch() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://mulung--bolaboka.repl.co/";
+        }   
+        function inews() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://embed.rctiplus.com/live/inews/inewsid";
+        }  
+        function rcti() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://sindikasi.inews.id/embed/video/YWdlbnQ9ZGVza3RvcCZ1cmw9aHR0cHMlM0ElMkYlMkZlbWJlZC5yY3RpcGx1cy5jb20lMkZsaXZlJTJGcmN0aS";
+        }   
+function rcti2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://rcti.livestreaming.repl.co";
+        }   
+        
+        function mnctv() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://embed.rctiplus.com/live/mnctv/inewsid";
+        }   
+        
+        function refreshPage() {
+      location.reload();
+        }
