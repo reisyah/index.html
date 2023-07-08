b94a8d2ba91249662a8ac7b023d68ca15c566d21
reisyah <138543445+reisyah@users.noreply.github.com>
2023-07-08 18:18:06 +0800

    Update function.js

--- function.js
+++ function.js
@@ -126,7 +126,7 @@ function p2thapcam4() {
 
 function p2soco1() {
             var frame = document.getElementById("vidio");
-            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/socolive/379816.m3u8";
+            frame.src = "https://arjunaaaaa.github.io/nyuri/pemutar/player2.html?file=https://code.vthanhtivi.pw/bongda/socolive/427562.m3u8";
 }
 
 function p2soco2() {
